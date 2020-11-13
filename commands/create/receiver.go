93afc6363c5c333fcb0f0cee65f7e77391b50b3a
Athenais Dussordet <ma.dussordet@gmail.com>
2020-11-13 11:38:22 +0100

    Fix: linter pass

--- commands/create/receiver.go
+++ commands/create/receiver.go
@@ -1,7 +1,9 @@
 package create
 
+//Receiver to receive command and send according message
 type Receiver struct{}
 
+//Answer sends channel message to discord bot according to the command
 func (r *Receiver) Answer(p *CharacterCreateCommandPayload) {
 	p.Session().ChannelMessageSend(p.Message.ChannelID, p.Answer)
 }
