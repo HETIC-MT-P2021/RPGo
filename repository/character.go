24f0d8b774c917edfea94de1d5ab121df1e384f6
Athenais Dussordet <ma.dussordet@gmail.com>
2020-11-13 11:00:57 +0100

    Fix: identation

--- repository/character.go
+++ repository/character.go
@@ -4,20 +4,20 @@ import "database/sql"
 
 //GetCharacterByDiscordUserID get a character by its discord user id (unique)
 func (repository *CharacterRepository) GetCharacterByDiscordUserID(discordUserID string) (*Character, error) {
-	row := repository.Conn.QueryRow("SELECT pc.id, pc.name, pc.class, pc.discord_user_id "+
-		"FROM p_character pc WHERE pc.discord_user_id=(?)", discordUserID)
-	var id int64
-	var name, class string
-	switch err := row.Scan(&id, &name, &class, &discordUserID); err {
+	row := repository.Conn.QueryRow(`
+		SELECT 
+		pc.id, 
+		pc.name, 
+		pc.class, 
+		pc.discord_user_id 
+		FROM p_character pc 
+		WHERE pc.discord_user_id=(?)`, discordUserID)
+	char := Character{}
+	switch err := row.Scan(&char.ID, &char.Name, &char.Class, &char.DiscordUserID); err {
 	case sql.ErrNoRows:
 		return nil, nil
 	case nil:
-		return &Character{
-			ID:            id,
-			Name:          name,
-			Class:         class,
-			DiscordUserID: discordUserID,
-		}, nil
+		return &char, nil
 	default:
 		return nil, err
 	}
@@ -25,7 +25,9 @@ func (repository *CharacterRepository) GetCharacterByDiscordUserID(discordUserID
 
 //Create saves a character in db
 func (repository *CharacterRepository) Create(character *Character) error {
-	stmt, err := repository.Conn.Prepare("INSERT INTO p_character (name, class, discord_user_id) VALUES (?,?,?)")
+	stmt, err := repository.Conn.Prepare(`
+	INSERT INTO p_character (name, class, discord_user_id) 
+	VALUES (?,?,?)`)
 	if err != nil {
 		return err
 	}
