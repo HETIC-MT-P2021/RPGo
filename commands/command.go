3e93f421bc4dcda9db54cd492cbb4a2551b4e843
Athenais Dussordet <ma.dussordet@gmail.com>
2020-11-13 11:38:22 +0100

    Fix: linter pass

--- commands/command.go
+++ commands/command.go
@@ -2,11 +2,13 @@ package commands
 
 import "github.com/bwmarrin/discordgo"
 
+//Command interface (command pattern)
 type Command interface {
 	Execute() error
 	Payload() *CommandPayload
 }
 
+//CommandPayload pattern for command information
 type CommandPayload interface {
 	Session() *discordgo.Session
 }
