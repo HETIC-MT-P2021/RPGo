fc866094f6479852df15ebb360635dc06b3f2e9d
Athenais Dussordet <ma.dussordet@gmail.com>
2020-12-07 12:16:04 +0100

    Update: refactor repository tests

--- repository/character_test.go
+++ repository/character_test.go
@@ -1,15 +1,16 @@
-package repository
+package repository_test
 
 import (
 	"database/sql"
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/HETIC-MT-P2021/RPGo/repository"
 	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-var c = &Character{
+var character = &repository.Character{
 	Name:          "First Character",
 	Class:         "ranger",
 	DiscordUserID: "1234",
@@ -24,28 +25,41 @@ func NewMock() (*sql.DB, sqlmock.Sqlmock) {
 	return db, mock
 }
 
-func TestCharacterRepository_GetCharacterByDiscordUserID_NotFound(t *testing.T) {
-	db, mock := NewMock()
-	repo := &CharacterRepository{db}
-	defer func() {
-		repo.Close()
-	}()
-
-	query := "SELECT pc.id, pc.name, pc.class, pc.discord_user_id FROM p_character pc WHERE pc.discord_user_id = \\?"
-
-	rows := sqlmock.NewRows([]string{"id", "name", "class", "discord_user_id"})
-
-	mock.ExpectQuery(query).WithArgs(c.DiscordUserID).WillReturnRows(rows)
-
-	character, err := repo.GetCharacterByDiscordUserID(c.DiscordUserID)
-	assert.Empty(t, character)
-	assert.NoError(t, err)
+func TestCharacterRepository_GetCharacterByDiscordUserID(t *testing.T) {
+	for n, c := range map[string]struct {
+		query string
+		rows  *sqlmock.Rows
+		char  *repository.Character
+	}{
+		"should find existing character": {
+			query: "SELECT pc.id, pc.name, pc.class, pc.discord_user_id FROM p_character pc WHERE pc.discord_user_id = \\?",
+			rows:  sqlmock.NewRows([]string{"id", "name", "class", "discord_user_id"}).AddRow(character.ID, character.Name, character.Class, character.DiscordUserID),
+			char:  character,
+		},
+		"should not find existing character": {
+			query: "SELECT pc.id, pc.name, pc.class, pc.discord_user_id FROM p_character pc WHERE pc.discord_user_id = \\?",
+			rows:  sqlmock.NewRows([]string{"id", "name", "class", "discord_user_id"}),
+			char:  nil,
+		},
+	} {
+		t.Run(n, func(t *testing.T) {
+			db, mock := NewMock()
+			repo := &repository.CharacterRepository{Conn: db}
+			defer func() {
+				repo.Close()
+			}()
+			mock.ExpectQuery(c.query).WithArgs(character.DiscordUserID).WillReturnRows(c.rows)
+			character, err := repo.GetCharacterByDiscordUserID(character.DiscordUserID)
+			assert.Equal(t, character, c.char)
+			assert.NoError(t, err)
+		})
+	}
 }
 
 func TestCharacterRepository_Create(t *testing.T) {
 
 	db, mock := NewMock()
-	repo := &CharacterRepository{db}
+	repo := &repository.CharacterRepository{Conn: db}
 	defer func() {
 		repo.Close()
 	}()
@@ -53,29 +67,10 @@ func TestCharacterRepository_Create(t *testing.T) {
 	query := "INSERT INTO p_character \\(name, class, discord_user_id\\) VALUES \\(\\?,\\?,\\?\\)"
 
 	prep := mock.ExpectPrepare(query)
-	prep.ExpectExec().WithArgs(c.Name, c.Class, c.DiscordUserID).WillReturnResult(sqlmock.NewResult(1, 1))
+	prep.ExpectExec().WithArgs(character.Name, character.Class, character.DiscordUserID).WillReturnResult(sqlmock.NewResult(1, 1))
 
-	err := repo.Create(c)
+	err := repo.Create(character)
 	assert.NoError(t, err)
 
-	assert.Equal(t, int64(1), c.ID)
-}
-
-func TestCharacterRepository_GetCharacterByDiscordUserID_Found(t *testing.T) {
-	db, mock := NewMock()
-	repo := &CharacterRepository{db}
-	defer func() {
-		repo.Close()
-	}()
-
-	query := "SELECT pc.id, pc.name, pc.class, pc.discord_user_id FROM p_character pc WHERE pc.discord_user_id = \\?"
-
-	rows := sqlmock.NewRows([]string{"id", "name", "class", "discord_user_id"}).
-		AddRow(c.ID, c.Name, c.Class, c.DiscordUserID)
-
-	mock.ExpectQuery(query).WithArgs(c.DiscordUserID).WillReturnRows(rows)
-
-	character, err := repo.GetCharacterByDiscordUserID(c.DiscordUserID)
-	assert.NotNil(t, character)
-	assert.NoError(t, err)
+	assert.Equal(t, int64(1), character.ID)
 }
